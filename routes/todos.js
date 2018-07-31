f90ff64a9951b76fa598270c33e860bcf3588a0d
ShangWong <wang.s.ai@outlook.jp>
2018-07-31 18:07:00 +0800

    initial commit

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,12 @@
 'use strict';
+var request = require('request');
 var router = require('express').Router();
 var AV = require('leanengine');
 
 var Todo = AV.Object.extend('Todo');
+var Task = AV.Object.extend('Task');
+
+var token = '';
 
 // 查询 Todo 列表
 router.get('/', function(req, res, next) {
@@ -27,14 +31,57 @@ router.get('/', function(req, res, next) {
   }).catch(next);
 });
 
+router.post('/key', function(req, res, next) {
+  var project_id = req.body.project;
+  var headers = {
+    'Authorization': 'Bearer 3dda56fbadbdb3e44d8038d0296517c7199b57a9'
+  }
+  var options = {
+    url: 'https://beta.todoist.com/API/v8/tasks?project_id=' + project_id,
+    method: 'GET',
+    headers: headers,
+    json: true
+  }
+  
+  request(options, function (error, response, body) {
+    for (const onetask of body) {
+        var task = new Task();
+        task.set('project_id', onetask.project_id);
+        task.set('content', onetask.content);
+        task.set('completed', onetask.completed);
+        task.set('priority', onetask.priority);
+        task.set('url', onetask.url);
+        task.save().then(function(task) {
+        }).catch(next);
+    }
+    res.redirect('/todos')
+  });
+});
 // 新增 Todo 项目
 router.post('/', function(req, res, next) {
-  var content = req.body.content;
-  var todo = new Todo();
-  todo.set('content', content);
-  todo.save().then(function(todo) {
-    res.redirect('/todos');
-  }).catch(next);
+  token = req.body.content;
+  var headers = {
+    'Authorization': 'Bearer ' + token
+  }
+  var options = {
+    url: 'https://beta.todoist.com/API/v8/projects',
+    method: 'GET',
+    headers: headers,
+    json: true
+  }
+
+  request(options, function (error, response, body) {
+    for (const project of body) {
+        var todo = new Todo();
+        todo.set('content', project.name);
+        todo.set('key', project.id);
+        todo.save().then(function(todo) {
+        }).catch(next);
+    }
+
+    res.redirect('/todos')
+  });
+
 });
 
 module.exports = router;
